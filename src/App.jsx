08e4d03d1901abfbd08894e00633925809aae22e
hmel1990 <hmel408757595@gmail.com>
2025-07-02 13:58:15 +0300

    Weather

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './components/Weather.jsx'
+import Weather from "./components/Weather.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Weather />
+    </>
+  )
+}
+
+export default App
